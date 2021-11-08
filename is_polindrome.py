ec94ab519ee88cf688c5fcb318c200e55dab4687
Muhammadaziz Usmonjonov <usmonjonovmuhammadaziz63@gmail.com>
2021-11-08 07:23:51 -0500

    Edited file

--- is_polindrome.py
+++ is_polindrome.py
@@ -1,3 +1,7 @@
 palindrome_text = input('Please enter only text: ')
 rev_palindrome_text = palindrome_text[::-1]
-print('reversed string')
+print('reversed string',rev_palindrome_text)
+if  palindrome_text == rev_palindrome_text:
+    print('String is palindrome')
+else:
+    print('String is not palindrome')
